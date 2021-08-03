c1fea666fef77f4aadcdaa098292144606591bc5
Natalia_Sherchenkova <tataterer@gmail.com>
2021-08-03 21:59:03 +0100

    Created generic components and linked them into the App.js

--- mern-exercise-tracker/src/components/create-user.component.js
+++ mern-exercise-tracker/src/components/create-user.component.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
-export default class ExercisesList extends Component {
+export default class CreateUser extends Component {
   render() {
     return (
       <div>
-        <p>Exercises List Component!</p>
+        <p>Create User Component!</p>
       </div>
     )
   }
--- mern-exercise-tracker/src/components/navbar.component.js
+++ mern-exercise-tracker/src/components/navbar.component.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link }, from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
 
@@ -7,7 +7,7 @@ export default class Navbar extends Component {
     return (
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
         <Link to="/" className="navbar-brand">ExerTracker</Link>
-        <div className="collapse navbar-collapse">
+        <div className="collapse navbar-collapse" >
         <ul className="navbar-nav mr-auto">
           <li className="navbar-item">
           <Link to="/" className="nav-link">Exercises</Link>
